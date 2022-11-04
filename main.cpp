fd01f71ce1f4c6a84cbbdf7d083fa1f615c9bcd7
azamd7 <hi.ayazahmed@gmail.com>
2022-11-04 18:21:50 +0530

    basic first impl

--- main.cpp
+++ main.cpp
@@ -129,6 +129,19 @@ class graphList
 {
  public:
   VNode *Head, *Tail;
+
+  graphList()
+  {
+    Head = new VNode();
+    Tail = new VNode();
+    this->Head->val = INT_MIN;
+    this->Tail->val = INT_MAX;
+    this->Head->enext = NULL;
+    this->Tail->enext = NULL;
+    this->Head->vnext = Tail;
+    this->Tail->vnext = NULL;
+  }
+
   // creation of new ENode
   ENode* createE(int key){
     ENode * newe = new ENode();
@@ -173,56 +186,56 @@ class graphList
 
   // Find pred and curr for VNode(key)     
   void locateVPlus(VNode *startV, VNode ** n1, VNode ** n2, int key){
-        VNode *succv, *currv, *predv;
-        retry:
-        while(true){
-          predv = startV;
-          currv = predv->vnext.load();
-          while(true){
-          succv = currv->vnext.load();
-          while(currv->vnext.load() != NULL && is_marked_ref((long) succv) && currv->val < key ){ 
-            ReportDeleteVertex(currv);
-            if(!predv->vnext.compare_exchange_strong(currv, (VNode *)get_unmarked_ref((long)succv), memory_order_seq_cst))
-              goto retry;
-            currv = (VNode *)get_unmarked_ref((long)succv);
-            succv = currv->vnext.load(); 
-          }
-          if(currv->val >= key){
-            (*n1) = predv;
-            (*n2) = currv;
-            return;
-          }
-          predv = currv;
-          currv = succv;
-          }
-      }  
+    VNode *succv, *currv, *predv;
+    retry:
+    while(true){
+      predv = startV;
+      currv = predv->vnext.load();
+      while(true){
+      succv = currv->vnext.load();
+      while(currv->vnext.load() != NULL && is_marked_ref((long) succv) && currv->val < key ){ 
+        ReportDeleteVertex(currv);
+        if(!predv->vnext.compare_exchange_strong(currv, (VNode *)get_unmarked_ref((long)succv), memory_order_seq_cst))
+          goto retry;
+        currv = (VNode *)get_unmarked_ref((long)succv);
+        succv = currv->vnext.load(); 
+      }
+      if(currv->val >= key){
+        (*n1) = predv;
+        (*n2) = currv;
+        return;
+      }
+      predv = currv;
+      currv = succv;
+      }
+    }  
   } 
 
 
   // add a new vertex in the vertex-list
-  bool AddV(int key, int n)
+  bool AddVertex(int key, int n)
   {
-        VNode *predv, *currv;
-        while(true)
-        {
-          locateVPlus(Head, &predv, &currv, key); // find the location, <pred, curr>
-          if(currv->val == key)
-          {
-            ReportInsertVertex(currv);
-            return false; // key already present
-          }       
-          else{
-            VNode *newv = createV(key, n); // create a new vertex node
-            newv->vnext.store(currv);  
-            if(atomic_compare_exchange_strong(&predv->vnext, &currv, newv)) {// added in the vertex-list
-                return true;
-            }
-            } 
+    VNode *predv, *currv;
+    while(true)
+    {
+      locateVPlus(Head, &predv, &currv, key); // find the location, <pred, curr>
+      if(currv->val == key)
+      {
+        ReportInsertVertex(currv);
+        return false; // key already present
+      }       
+      else{
+        VNode *newv = createV(key, n); // create a new vertex node
+        newv->vnext.store(currv);  
+        if(atomic_compare_exchange_strong(&predv->vnext, &currv, newv)) {// added in the vertex-list
+            return true;
         }
+      } 
     }
+  }
 
   // Deletes the vertex from the vertex-list
-  bool RemoveV(int key){
+  bool RemoveVertex(int key){
     VNode *predv, *currv, *succv;
     while(true){
       locateVPlus(Head, &predv, &currv, key);
@@ -242,6 +255,26 @@ class graphList
     return true;
   }
 
+  // Contains VNode   
+  bool ContainsV(int key){
+    VNode *currv = Head;
+    while(currv->vnext.load() && currv->val < key){
+      currv =  (VNode*)get_unmarked_ref((long)currv->vnext.load());
+    }
+    VNode *succv = currv->vnext.load();
+	  if((currv->vnext.load()) && currv->val == key && !is_marked_ref((long) succv)){
+	    ReportInsertVertex(currv);
+      return true;
+	  }   
+	  else {
+      if(is_marked_ref((long)succv))
+        ReportDeleteVertex(currv);
+	    return false;
+    }   
+  }
+
+  
+      
 };
 
 int main()
