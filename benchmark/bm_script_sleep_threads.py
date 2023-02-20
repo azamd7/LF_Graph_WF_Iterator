c22d2092502e42bc747a1cc11ec3ad55d5d68c76
pratiks14 <pratikshetty14@gmail.com>
2023-02-21 00:03:41 +0530

    sleep variant added

--- benchmark/bm_script_sleep_threads.py
+++ benchmark/bm_script_sleep_threads.py
@@ -12,21 +12,18 @@ now = datetime.now() # current date and time
 date_time_obj =now.strftime('%H_%M_%S') 
 
 
-#threads = 52 
-#######
-threads = 8
-#######
-algos = [ "report_tt_ss_ext" ]
-
+#threads = [13,26,52,78,102] 
+sleep_threads_lst = [13, 26, 39, 52, 65, 78]
+sleep_time = 30 #in ms
+threads = 102
+#threads = [4,8]
+algos = [ "icdcn_sleep_resp" , "report_sleep_resp" ]
 debug = False
 main_file = "main.cpp"
 iterations = 7
 test_duration = "10" #no of sec before stop executions
-init_vertices = [10**4 * i for i in range(4,7)]
-#######
-#init_vertices = [10**4 * i for i in range(4,7)]
-#######
-init_edges = [2 * i for i in init_vertices]
+init_vertices = str(10**4)
+init_edges = str(2 * (10**4))
 
 #files
 maxt_output_file_fmt = '../output/{0}_op_' + date_time_obj +"_maxt_{1}" +  '.csv'
@@ -35,7 +32,7 @@ script_log_file = "../script_log/" + date_time_obj + ".txt"
 
 #commands
 cmd1 = "g++ -std=c++11 -pthread -O3 -o ../sources/{0}/a.out ../sources/{0}/" + main_file
-cmd2 = "../sources/{0}/a.out ../log/{1} {2} {3} {4} {5}"
+cmd2 = "../sources/{0}/a.out ../log/{1} {2} {3} {4} {5} {6} {7}"
 
 
 # * 0->add vertex
@@ -47,17 +44,15 @@ cmd2 = "../sources/{0}/a.out ../log/{1} {2} {3} {4} {5}"
 # * 6->snapshot
 
 dist_probs ={ 
-            #"loopup_int" : [3,2,3,2,45,45,0],
-            #"update_int" : [13,12,13,12,25,25,0]
-            "update_int_2" : [16,9,16,9,25,25,0]
-            #"lookup_int_2" : [4,1,4,1,45,45,0]
+            "loopup_int" : [3,2,3,2,45,45,0],
+            "update_int" : [13,12,13,12,25,25,0]
             }
 
 with open(script_log_file, 'w+') as log_f_object:
     for key in dist_probs.keys(): 
         print("\n\n\n\n\n\nProbablity Dist: "+ key +" " + str( dist_probs[key])  ,file = log_f_object,flush = True)
         
-        for i in range(2,11,2):
+        for i in [2,6,10]:
             dist_prob = dist_probs[key].copy()
             print("\n\nSnapshot Dist: "+str(i)  ,file = log_f_object,flush = True)
             if(i != 0):
@@ -68,7 +63,7 @@ with open(script_log_file, 'w+') as log_f_object:
             maxt_output_file = maxt_output_file_fmt.format(key , "ss_" + str(i))
             avgt_output_file = avgt_output_file_fmt.format(key , "ss_" + str(i))
             with open( maxt_output_file, 'w+') as f_object:
-                lst = ["Init Nodes" ]
+                lst = ["Sleep Threads" ]
                 for algo in algos:
                     lst.append(algo)
                 
@@ -77,7 +72,7 @@ with open(script_log_file, 'w+') as log_f_object:
                 writer_object.writerow(lst)
 
             with open( avgt_output_file, 'w+') as f_object:
-                lst = ["Init Nodes"  ]
+                lst = ["Sleep Threads"  ]
                 for algo in algos:
                     lst.append(algo)
                 writer_object = writer(f_object)
@@ -101,12 +96,10 @@ with open(script_log_file, 'w+') as log_f_object:
                         proc.wait()
 
                     number_of_triangles_parr = 0
-                    for p in range(len(init_vertices)):
-                        init_node_cnt = init_vertices[p]
-                        init_edge_cnt = init_edges[p]
-                        max_lst = [init_node_cnt]
-                        avg_lst = [init_node_cnt]
-                        print("Init node cnt : " + str(init_node_cnt),file = log_f_object,flush = True)
+                    for sleep_threads in sleep_threads_lst :
+                        max_lst = [sleep_threads]
+                        avg_lst = [sleep_threads]
+                        print("Sleep Thread cnt : " + str(sleep_threads),file = log_f_object,flush = True)
                         
                         for algo in algos:
                             cmd = cmd2
@@ -116,7 +109,7 @@ with open(script_log_file, 'w+') as log_f_object:
                             if debug:
                                 cmd += " debug"
                             print("Algo : "+ algo,file = log_f_object,flush = True)
-                            cmd = cmd.format(algo,date_time_obj,str(threads),test_duration,str(init_node_cnt),str(init_edge_cnt))
+                            cmd = cmd.format(algo,date_time_obj,str(threads),test_duration,init_vertices,init_edges,sleep_threads ,sleep_time)
                             avg_time_taken_list = []
                             max_time_taken_list = []
 
@@ -130,23 +123,22 @@ with open(script_log_file, 'w+') as log_f_object:
                                 print(std_output)
                                 if std_err is not None:
                                     raise Exception(std_err)
-                                avg_time , max_time , avg_nodes = "","",""
+                                avg_time , max_time = "",""
                                 if len(std_output.decode().split()) > 1:
-                                    avg_time, max_time, avg_nodes, _ = std_output.decode().split('\n')
+                                    avg_time, max_time, _ = std_output.decode().split('\n')
                                 if not avg_time:
                                     lst.append("")#empty for that thread
-                                    print("No o/p for init node cnt : " + str(init_node_cnt) + " and  Iteration :  " + str(i),file = log_f_object,flush = True)
+                                    print("No o/p for sleep thread cnt : " + str(sleep_threads) + " and  Iteration :  " + str(i),file = log_f_object,flush = True)
                                     continue
                                 print("Average Snapshot: " + str(avg_time),file = log_f_object,flush = True)
                                 avg_time_taken_list.append(float(avg_time))
 
                                 if not max_time:
                                     lst.append("")
-                                    print("No max_time in o/p for init node cnt : " + str(init_node_cnt)+ " and  Iteration : " + str(i),file = log_f_object,flush = True)
+                                    print("No max_time in o/p for sleep thread cnt : " + str(sleep_threads)+ " and  Iteration : " + str(i),file = log_f_object,flush = True)
                                     continue
                                 print("Max Snapshot : " + str(max_time),file = log_f_object,flush = True)
                                 max_time_taken_list.append(float(max_time))
-                                print("Average Nodes in snapshots : " + str(avg_nodes),file = log_f_object,flush = True)
                                 print("\n",file= log_f_object,flush = True)
                                 print(file= log_f_object,flush = True)
                                 print(file= log_f_object,flush = True)
